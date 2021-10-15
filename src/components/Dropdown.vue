ae74f3635416373efd17a870b03820aac84b231e
l.shuang <lsbright@qq.com>
2021-10-15 11:16:55 +0800

    Dropdown组件点击外部区域时自动隐藏

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
       {{ title }}
     </a>
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 
 export default defineComponent({
   name: 'Dropdown',
@@ -25,9 +25,31 @@ export default defineComponent({
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+
+    // 用于获取 dropdownRef 的 DOM 节点
+    const dropdownRef = ref<null | HTMLElement>(null)
+    const handler = (e: MouseEvent) => {
+      // 判断节点是否存在
+      if (dropdownRef.value) {
+        // node.contains(otherNode)判断节点是否包含另一个节点
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
+
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef // 返回的 dropdownRef 与 ref="dropdownRef" 相同，页面挂载时，dropdownRef 便就是想要获取的 ref="dropdownRef" DOM 节点
     }
   }
 })
