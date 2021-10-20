b4b5b39644d925c082b459cff64028249a92c00b
l.shuang <lsbright@qq.com>
2021-10-20 17:29:50 +0800

    修改

--- src/router.ts
+++ src/router.ts
@@ -16,8 +16,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: Login,
-      meta: { redirectAlreadyLogin: true }
+      component: Login
     }
   ]
 })
