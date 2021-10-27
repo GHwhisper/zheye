f507167c30bdcf7d3a93cfede12686bc99bd69b4
l.shuang <lsbright@qq.com>
2021-10-27 11:31:46 +0800

    ColumnDetail页面、PostList组件

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
-import ColumnDetail from '@/components/ColumnDetail.vue'
+import ColumnDetail from '@/views/ColumnDetail.vue'
 
 const routerHistory = createWebHistory()
 
