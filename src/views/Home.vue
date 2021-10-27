e1ec7c870f6a1586c6ac098b7e8bcdf361572f9a
l.shuang <lsbright@qq.com>
2021-10-27 16:19:57 +0800

    vuex的getters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <column-list :list="list"></column-list>
+  <div class="home-page">
+    <h2>{{ biggerColumnLen }}</h2>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <img src="../assets/callout.svg" alt="callout" class="w-50">
+          <h2 class="font-weight-light">随心写作，自由表达</h2>
+          <p>
+            <a href="#" class="btn btn-primary my-2">开始写文章</a>
+          </p>
+        </div>
+      </div>
+    </section>
+    <h4 class="font-weight-bold text-center">发现精彩</h4>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,9 +31,11 @@ export default defineComponent({
   setup () {
     const store = useStore<GlobalDataProps>()
     const list = computed(() => store.state.columns)
+    const biggerColumnLen = computed(() => store.getters.biggerColumnLen)
 
     return {
-      list
+      list,
+      biggerColumnLen
     }
   }
 })
