5d2e4969bad25f282526aabb6f26d925c6d2fc05
liangshuang <lsbright@qq.com>
2021-12-27 16:46:03 +0800

    配置变量大写

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,12 @@
 module.exports = {
   devServer: {
     proxy: {
-      [process.env.Vue_APP_BASE_API]: {
+      [process.env.VUE_APP_BASE_API]: {
         // target: 'http://apis.imooc.com/api', // 该接口需要 icode
         target: 'http://api.vikingship.xyz/api', // 测试接口
         changeOrigin: true,
         pathRewrite: {
-          ['^' + process.env.Vue_APP_BASE_API]: ''
+          ['^' + process.env.VUE_APP_BASE_API]: ''
         }
       }
     }
